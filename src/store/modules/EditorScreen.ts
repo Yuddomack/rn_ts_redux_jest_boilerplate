291a65add3bede821c6adfbf7a756458a4e90d40
yuddomack <darr880406@gmail.com>
2019-09-02 14:51:39 +0900

    update: update action type 'any' for test

--- src/store/modules/EditorScreen.ts
+++ src/store/modules/EditorScreen.ts
@@ -16,21 +16,28 @@ interface changeText {
     text: string
 }
 
+interface changeColor {
+    type: typeof CHANGE_TEXT
+    color: string
+}
+
 type ActionTypes = 
-    | changeText;
+    | changeText
+    | changeColor
 
 // 액션 생섬함수 정의
-export const changeText = (text: string = "") => ({ type: CHANGE_TEXT, text }); // onChange니까 edit보단 change가 나을듯
+export const changeText = (text: string = ""): ActionTypes => ({ type: CHANGE_TEXT, text }); // onChange니까 edit보단 change가 나을듯
 
 // 초기상태 정의
-const initialState: IState = {
+export const initialState: IState = {
     text: "",
 }; // state는 되도록 얕은 객체로
 
 // 리듀서 작성
+// https://stackoverflow.com/questions/55636963/property-does-not-exist-on-type
 export default function EditorScreen(
     state = initialState, 
-    action: ActionTypes
+    action: any | ActionTypes // any for empty
 ): IState {
     switch (action.type) {
         case CHANGE_TEXT:
