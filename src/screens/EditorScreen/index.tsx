a7cd87533d7a421982deb2a48a0eec32a23616e7
yuddomack <darr880406@gmail.com>
2019-09-04 11:15:02 +0900

    update: EditorScreen -> EditorScreen Container

--- src/screens/EditorScreen/index.tsx
+++ src/screens/EditorScreen/index.tsx
@@ -1,3 +1,5 @@
+// Container
+
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {
@@ -5,32 +7,35 @@ import {
     TextInput,
     Text,
 } from 'react-native';
-import { IProps, changeText } from '../../store/modules/EditorScreen';
+import EditorScreen from './EditorScreen';
+import { IProps, onChangeText } from '../../store/modules/EditorScreen';
 
 interface Props extends IProps {
 
 }
+
 interface States {
 
 }
 
-class EditorScreen extends Component<Props, States>{ // props, state에 대한 redux -> ts로 인터페이스를 받아와야할듯
+class EditorScreenContainer extends Component<Props, States>{ // props, state에 대한 redux -> ts로 인터페이스를 받아와야할듯
 
     constructor(props: Props){
         super(props);
     }
 
     _changeText = (text: string) => {
-        this.props.changeText(text);
+        this.props.onChangeText(text);
     }
 
     render(){
+        const { text } = this.props;
+        const { _changeText } = this;
+
         return (
-            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>에디터</Text>
-                <Text>{this.props.text}</Text>
-                <TextInput onChangeText={this._changeText.bind(this)} value={this.props.text}/>
-            </View>
+            <EditorScreen 
+                text={text}
+                onChangeText={_changeText} />
         )
     }
 }
@@ -42,7 +47,7 @@ const mapStateToProps = state => ({
   
   // props 로 넣어줄 액션 생성함수
   const mapDispatchToProps = dispatch => ({
-    changeText: (text: string) => dispatch(changeText(text)),
+    changeText: (text: string) => dispatch(onChangeText(text)),
   });
 
 export default connect(
